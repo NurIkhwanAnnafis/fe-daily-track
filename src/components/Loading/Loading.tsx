83c0db253b8e3cf173200c6516472eda4946c946
ikhwan <nur.ikhwan.annafis@gmail.com>
2026-03-13 16:34:13 +0700

    feat: update component loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -4,9 +4,24 @@ import { LoadingOutlined } from "@ant-design/icons"
 
 const Loading: React.FC = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center bg-neutral-700/50">
-      <Spin indicator={<LoadingOutlined spin />} size="large" />
+    <div
+      className="bg-neutral-900/80 fixed left-0 top-0 z-50 h-full w-full text-neutral-900"
+    >
+      <div
+        className="bg-white my-[5%] mx-auto w-64 px-2 py-5 text-center rounded"
+      >
+        <div className="flex justify-center mb-4 text-primary-500">
+          <Spin indicator={<LoadingOutlined spin className="text-5xl!" />} />
+        </div>
+        <p className="text-xl font-medium text-neutral-700 mb-2">
+          Loading
+        </p>
+        <p className="text-base font-normal text-neutral-500">
+          Please wait...
+        </p>
+      </div>
     </div>
+
   )
 }
 
